2cad2a83de7a57063b4126dbef0a2f9975316ef2
Jake Gillberg <jake.gillberg@gmail.com>
2018-09-26 23:31:47 +0000

    refactor and make things (subjectivly) prettier

---
+++ contracts/interfaces/TokenInterface.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.24;
+
+interface TokenInterface {
+  function transfer(address _to, uint256 _value) external returns (bool success);
+  function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+  function approve(address _spender, uint256 _value) external returns (bool success);
+}
--- contracts/model/TokenInterface.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.4.24;
-
-interface TokenInterface {
-  function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
-}
