096dbb272f89a4bc6806539866e4ca77b042756e
Jake Gillberg <jake.gillberg@gmail.com>
2018-09-26 11:52:55 +0000

    Switch to truffle (mocha) test framework

--- contracts/CirclesPerson.sol
+++ contracts/CirclesPerson.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "src/main/model/PersonInterface.sol";
+import "./model/PersonInterface.sol";
 
 contract CirclesPerson is PersonInterface {
 
---
+++ test/helpers/assertRevert.js
@@ -0,0 +1,18 @@
+// https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/test/helpers/assertRevert.js
+
+const should = require('chai')
+  .should();
+
+async function assertRevert (promise) {
+  try {
+    await promise;
+  } catch (error) {
+    error.message.should.include('revert', `Expected "revert", got ${error} instead`);
+    return;
+  }
+  should.fail('Expected revert not received');
+}
+
+module.exports = {
+  assertRevert,
+};
