1f8b2b7a67d52ddfb22df66b191966e957468fb8
David Terry <me@xwvvvvwx.com>
2018-10-10 15:45:20 +0300

    CirclesPerson: add source param to exchangeTransfer

--- contracts/CirclesPerson.sol
+++ contracts/CirclesPerson.sol
@@ -15,7 +15,6 @@ contract CirclesPerson {
     owner = msg.sender;
   }
 
-  //TODO: Update owner mechanism
   function updateExchangeInput( address _token
                               , bool _isTrusted ) public
                                                     returns (bool success) {
@@ -44,36 +43,41 @@ contract CirclesPerson {
   }
 
   // TODO: modifier instead of internal function? and spellcheck, lol
-  function _exchangePrerequisates( address _offeredToken
+  function _exchangePrerequisates( address _source
+                                 , address _offeredToken
                                  , address _desiredToken
-		                 , uint256 _value         ) internal {
+                                 , uint256 _value ) internal {
 
     require( isExchangeApproved(_offeredToken, _desiredToken)
            , "Offered token not accepted at this time"
     );
 
     require( TokenInterface(_offeredToken)
-               .transferFrom(msg.sender, this, _value)
+               .transferFrom(_source, this, _value)
            , "Unable to transfer offered token"
     );
   }
 
   function exchangeTransfer( address _offeredToken
                            , address _desiredToken
-                           , address _destination, uint256 _value ) public {
+                           , address _source
+                           , address _destination 
+                           , uint256 _value ) public {
 
-    _exchangePrerequisates(_offeredToken, _desiredToken, _value);
+    _exchangePrerequisates(_source, _offeredToken, _desiredToken, _value);
 
-    require( TokenInterface(_desiredToken).transfer(_destination, _value)
+    require( TokenInterface(_desiredToken).transferFrom(this, _destination, _value)
            , "Unable to transfer desired token"
     );
   }
 
   function exchangeApprove( address _offeredToken
                           , address _desiredToken
-                          , address _destination, uint256 _value ) public {
+                          , address _source
+                          , address _destination
+                          , uint256 _value ) public {
 
-    _exchangePrerequisates(_offeredToken, _desiredToken, _value);
+    _exchangePrerequisates(_source, _offeredToken, _desiredToken, _value);
 
     require( TokenInterface(_desiredToken).approve(_destination, _value)
            , "Unable to approve transfer of desired token"
