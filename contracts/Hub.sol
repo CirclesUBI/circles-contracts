fbda2d59dfb2be0579d0f228e89ea371d2e6c537
anon <sarah.anne.friend@gmail.com>
2018-12-19 22:51:10 -0500

    Further work in Hub, todo notes

--- contracts/Hub.sol
+++ contracts/Hub.sol
@@ -2,12 +2,26 @@ pragma solidity ^0.4.24;
 
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import "./Token.sol";
 
-contract CirclesHub is Ownable {
+//update issuance
+//update demurrage
+//update limit_epoch
+//role of validators
+//registrar to be able to change hub?
+//if we do a registrar to be able to swap out hub, do we care about token logic?
+//standard token params
+//finish update function in token
+
+
+contract Hub is Ownable {
     using SafeMath for uint256;
 
     uint256 public issuanceRate = 1736111111111111; // ~1050 tokens per week
+    //unint256 public demurrageRate = ??;
+    uint256 public defaultDecimals = 18;
 
+    // do users manager their own token weights, and is this the best way to handle this param?
     uint constant LIMIT_EPOCH = 3600;
 
     struct EdgeWeight {
@@ -16,33 +30,44 @@ contract CirclesHub is Ownable {
         uint lastTouched;
     }
 
-    mapping (address => CirclesToken) public userToToken;
+    mapping (address => Token) public userToToken;
     mapping (address => address) public tokenToUser;
-
     mapping (address => bool) isValidator;
-
     mapping (address => mapping (address => EdgeWeight)) public edges;
+
+    event Signup(address indexed user);
+    event Trust(address indexed from, address indexed to, uint256 limit);
+    event RegisterValidator(address indexed validator);
+
+
 
     function time() returns (uint) { return block.timestamp; }
 
     // No exit allowed. Once you create a personal token, you're in for good.
-    function join() note {
+    function signup() external returns (bool) {
         assert(address(userToToken[msg.sender]) == 0);
-        var token = new CirclesToken(msg.sender);
+
+        // need to pass in default params here ...
+        Token token = new Token(msg.sender);
         userToToken[msg.sender] = token;
         tokenToUser[address(token)] = msg.sender;
+
+        emit Signup(msg.sender);
+        return true;
     }
 
-    function register() note {
-        isValidator[msg.sender] = true;
+    // no validation on the registering of validators
+    function registerValidator(address validator) external {
+        isValidator[validator] = true;
+        emit RegisterValidator(validator);
     }
 
     // Trust does not have to be reciprocated.
     // (e.g. I can trust you but you don't have to trust me)
-    function trust(address node, bool yes, uint limit) note {
-        assert(address(tokenToUser[node]) != 0 || isValidator[node]);
-        edges[msg.sender][node] = yes ? EdgeWeight(limit, 0, time()) : EdgeWeight(0, 0, 0);
-
+    function trust(address toTrust, bool yes, uint limit) {
+        assert(address(tokenToUser[toTrust]) != 0 || isValidator[toTrust]);
+        edges[msg.sender][toTrust] = yes ? EdgeWeight(limit, 0, time()) : EdgeWeight(0, 0, 0);
+        emit Trust(msg.sender, toTrust, limit);
     }
 
     // Starts with msg.sender then ,
@@ -95,7 +120,7 @@ contract CirclesHub is Ownable {
                 // from the current node to the msg.sender
                 if (tokenIndex + 1 < tokens.length) {
 
-                    var nextToken = CirclesToken(tokens[tokenIndex + 1]);
+                    var nextToken = Token(tokens[tokenIndex + 1]);
                     nextToken.transferFrom(node, msg.sender, wad);
                 }
                 tokenIndex++;
