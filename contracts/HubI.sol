1e587cd2cc2425356ad72fe9c487990bb55d18f5
anon <sarah.anne.friend@gmail.com>
2019-01-10 19:46:48 -0500

    Silence Compiler

--- contracts/HubI.sol
+++ contracts/HubI.sol
@@ -2,4 +2,7 @@ pragma solidity ^0.4.24;
 
 contract HubI {
     function issuanceRate() public returns (uint256);
+    function demurrageRate() public returns (uint256);
+    function decimals() public returns (uint8);
+    function symbol() public returns (string);
 }
