b94534b1fcee655e84673384dd85b161a2243850
anon <sarah.anne.friend@gmail.com>
2019-01-10 19:54:01 -0500

    Clean up whitespace

--- contracts/HubFactory.sol
+++ contracts/HubFactory.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.4.24;
 import "./Hub.sol";
 
 contract HubFactory {
+
     event Fork(address indexed newHub, address newHubOwner);
 
-    function fork(uint256 issuance, uint256 demurrage, uint8 decimals, string symbol,	uint256 limitEpoch) public returns (bool) {
+    function fork(
+        uint256 issuance, uint256 demurrage, uint8 decimals, string symbol, uint256 limitEpoch
+    ) public returns (bool) {
         Hub newHub = new Hub(msg.sender, issuance, demurrage, decimals, symbol, limitEpoch);
 	    emit Fork(address(newHub), msg.sender);
 	    return true;
