4f190d089d9230e603a5672d0bbf39cffbca350d
anon <sarah.anne.friend@gmail.com>
2019-01-16 19:59:00 -0500

    Rename fork and migrate

--- contracts/HubFactory.sol
+++ contracts/HubFactory.sol
@@ -4,13 +4,13 @@ import "./Hub.sol";
 
 contract HubFactory {
 
-    event Fork(address indexed newHub, address newHubOwner);
+    event Spawn(address indexed newHub, address newHubOwner);
 
-    function fork(
+    function spawn(
         uint256 issuance, uint256 demurrage, uint8 decimals, string memory symbol, uint256 limitEpoch
     ) public returns (bool) {
         Hub newHub = new Hub(msg.sender, issuance, demurrage, decimals, symbol, limitEpoch);
-	    emit Fork(address(newHub), msg.sender);
+	    emit Spawn(address(newHub), msg.sender);
 	    return true;
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var HubFactory = artifacts.require("./HubFactory.sol");
+const HubFactory = artifacts.require("./HubFactory.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(HubFactory);
+module.exports = async function(deployer) {
+  await deployer.deploy(HubFactory);
+  const hubFactory = await HubFactory.deployed()
+  return hubFactory.spawn(1736111111111111, 0, 18, 'CRC', 3600);
 };
