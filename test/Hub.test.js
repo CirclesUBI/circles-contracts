1b44770acbd14598aae7ea16244c6679393e0b84
anon <sarah.anne.friend@gmail.com>
2019-01-18 20:19:31 -0500

    More permissions test

--- test/Hub.test.js
+++ test/Hub.test.js
@@ -1,14 +1,13 @@
 const BigNumber = web3.utils.BN;
+const { assertRevert } = require('./helpers/assertRevert');
 
 require('chai')
   .use(require('chai-bn')(BigNumber))
   .should();
 
-//BigNumber = require('bignumber.js')
-
 const Hub = artifacts.require('Hub');
 
-contract('Hub', function ([_, systemOwner, attacker]) {
+contract('Hub', ([_, systemOwner, attacker]) => {
   let hub = null;
 
   const _issuance = new BigNumber(1736111111111111);
@@ -17,27 +16,80 @@ contract('Hub', function ([_, systemOwner, attacker]) {
   const _symbol = 'CRC';
   const _limitEpoch = new BigNumber(3600);
 
-  beforeEach(async function () {
+  beforeEach(async () => {
     hub = await Hub.new(systemOwner, _issuance, _demurrage, _decimals, _symbol, _limitEpoch);
   });
 
-  it('has an issuance rate', async function () {
+  it('has the correct owner', async () => {
+    (await hub.owner()).should.be.equal(systemOwner);
+  });
+
+  it('attacker cannot change owner', async () => {
+    await assertRevert(hub.changeOwner(attacker, { from: attacker }))
+  });
+
+  it('has an issuance rate', async () => {
     (await hub.issuanceRate()).should.be.bignumber.equal(_issuance);
   });
 
-  it('has a demurrage rate', async function () {
+  it('attacker cannot change issuance', async () => {
+    await assertRevert(hub.updateIssuance(42, { from: attacker }))
+  });
+
+  it('has a demurrage rate', async () => {
     (await hub.demurrageRate()).should.be.bignumber.equal(_demurrage);
   });
 
-  it('has a decimals setting', async function () {
+  it('attacker cannot change demurrage', async () => {
+    await assertRevert(hub.updateDemurrage(42, { from: attacker }))
+  });
+
+  it('has a decimals setting', async () => {
     (await hub.decimals()).should.be.bignumber.equal(_decimals);
   });
 
-  it('has a symbol', async function () {
+  it('has a symbol', async () => {
     (await hub.symbol()).should.be.equal(_symbol);
   });
 
-  it('has a limit epoch value', async function () {
+  it('attacker cannot change symbol', async () => {
+    await assertRevert(hub.updateSymbol('PLUM', { from: attacker }))
+  });
+
+  it('has a limit epoch value', async () => {
     (await hub.LIMIT_EPOCH()).should.be.bignumber.equal(_limitEpoch);
   });
+
+  it('attacker cannot change limit epoch', async () => {
+    await assertRevert(hub.updateLimitEpoch(42, { from: attacker }))
+  });
+
+  describe('owner can change system vars', async () => {
+    after(async () => { 
+      await hub.updateIssuance(_issuance, { from: systemOwner })
+      await hub.updateDemurrage(_demurrage, { from: systemOwner });
+      await hub.updateSymbol(_symbol, { from: systemOwner });
+      return hub.updateLimitEpoch(_limitEpoch, { from: systemOwner });
+    });
+
+    it('owner can change issuance', async () => {
+      await hub.updateIssuance(1, { from: systemOwner });
+      (await hub.issuanceRate()).should.be.bignumber.equal(new BigNumber(1));
+    });
+
+    it('owner can change demurrage', async () => {
+      await hub.updateDemurrage(1, { from: systemOwner });
+      (await hub.demurrageRate()).should.be.bignumber.equal(new BigNumber(1));
+    });
+
+    it('owner can change symbol', async () => {
+      await hub.updateSymbol('PLUM', { from: systemOwner });
+      (await hub.symbol()).should.be.equal('PLUM');
+    });
+
+    it('owner can change limit epoch', async () => {
+      await hub.updateLimitEpoch(1, { from: systemOwner });
+      (await hub.LIMIT_EPOCH()).should.be.bignumber.equal(new BigNumber(1));
+    });
+  })
 });
