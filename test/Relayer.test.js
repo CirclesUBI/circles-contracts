3c0322a4a8756df9454ee1b28790bab22894614f
anon <sarah.anne.friend@gmail.com>
2019-04-18 10:59:35 -0400

    More work on relayer tests

--- test/Relayer.test.js
+++ test/Relayer.test.js
@@ -1,11 +1,8 @@
-// https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/test/token/ERC20/ERC20.test.js
-
 const { assertRevert } = require('./helpers/assertRevert');
 const expectEvent = require('./helpers/expectEvent');
 
-// const ERC20Mock = artifacts.require('ERC20Mock');
 const Hub = artifacts.require('Hub');
-const Token = artifacts.require('Token');
+const Relayer = artifacts.require('TxRelay');
 
 const BigNumber = web3.utils.BN;
 
@@ -13,9 +10,10 @@ require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-contract('ERC20', function ([_, owner, recipient, anotherAccount, systemOwner]) {
+contract('Hub', ([_, systemOwner, attacker, relayer, tokenQwner]) => {
   const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
   let hub = null;
+  let relayer = null;
   let token = null;
 
   const _issuance = new BigNumber(1736111111111111);
@@ -26,17 +24,18 @@ contract('ERC20', function ([_, owner, recipient, anotherAccount, systemOwner]) 
   const _tokenName = 'MyCoin';
   const _initialPayout = new BigNumber(100);
 
-  // before(async () => {
-  //   hub = await Hub.new(systemOwner, _issuance, _demurrage, _decimals, _symbol, _limitEpoch);
-  // })
-
   beforeEach(async () => {
     hub = await Hub.new(systemOwner, _issuance, _demurrage, _decimals, _symbol, _limitEpoch, _initialPayout);
-    const signup = await hub.signup(_tokenName, { from: owner });// owner, 100);
-    token = await Token.at(signup.logs[0].args.token);
+    relayer = await Relayer.new(systemOwner, _issuance, _demurrage, _decimals, _symbol, _limitEpoch, _initialPayout);
+    // const signup = await hub.signup(_tokenName, { from: owner });
+    // token = await Token.at(signup.logs[0].args.token);
   });
 
-  describe('name is correctly set', () => {
+  describe('correctly relays a signup', () => {
+    before(async () => {
+      
+    })
+
     it('returns the total amount of tokens', async () => {
       (await token.totalSupply()).should.be.bignumber.equal(new BigNumber(100));
     });
