c0f707ebb8408deba0031c05f6a5edb362b362b8
anon <sarah.anne.friend@gmail.com>
2019-05-13 09:21:13 -0400

    Remove more decimals

--- test/Relayer.test.js
+++ test/Relayer.test.js
@@ -22,7 +22,6 @@ contract('Relayer', ([_, systemOwner, sender, api]) => {
 
     const _issuance = new BigNumber(1736111111111111);
     const _demurrage = new BigNumber(0);
-    const _decimals = new BigNumber(18);
     const _symbol = 'CRC';
     const _limitEpoch = new BigNumber(3600);
     const _tokenName = 'MyCoin';
@@ -33,7 +32,7 @@ contract('Relayer', ([_, systemOwner, sender, api]) => {
     let senderKeyPair;
 
     beforeEach(async () => {
-      hub = await Hub.new(systemOwner, _issuance, _demurrage, _decimals, _symbol, _limitEpoch, _initialPayout);
+      hub = await Hub.new(systemOwner, _issuance, _demurrage, _symbol, _limitEpoch, _initialPayout);
       relayer = await Relayer.new();
       metatxHandler = new MetaTxHandler(
         apiPrivKey,
