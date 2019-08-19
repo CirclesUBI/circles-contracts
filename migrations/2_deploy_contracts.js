2d7645cf2ea12ccc1c26d124c2b9877bbfafdd0c
anon <sarah.anne.friend@gmail.com>
2019-08-19 11:03:43 -0400

    Remove relayer, organizations, validators, and hubfactory

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const HubFactory = artifacts.require("./HubFactory.sol");
+const Hub = artifacts.require("./Hub.sol");
 
-module.exports = async function(deployer) {
-  await deployer.deploy(HubFactory);
-  const hubFactory = await HubFactory.deployed()
-  return hubFactory.spawn(1736111111111111, 0, 'CRC', 3600, 100);
+module.exports = async function(deployer, network, accounts) {
+  await deployer.deploy(Hub, accounts[0], 1736111111111111, 0, 'CRC', 3600, 100);
 };
