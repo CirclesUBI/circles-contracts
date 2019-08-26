4cb75910f912b78382cc8b9dfb7ebf80548cba1e
anon <sarah.anne.friend@gmail.com>
2019-08-26 02:44:48 -0400

    Tests passing without limit epoch

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Hub = artifacts.require("./Hub.sol");
 
 module.exports = async function(deployer, network, accounts) {
-  await deployer.deploy(Hub, accounts[0], 1736111111111111, 0, 'CRC', 3600, 100);
+  await deployer.deploy(Hub, accounts[0], 1736111111111111, 0, 'CRC', 100);
 };
