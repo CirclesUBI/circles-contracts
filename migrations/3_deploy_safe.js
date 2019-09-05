dc9301b5cb2a160ac005d8f686cdf4f320ef697f
anon <sarah.anne.friend@gmail.com>
2019-09-05 06:44:10 -0400

    Update migration

--- migrations/3_deploy_safe.js
+++ migrations/3_deploy_safe.js
@@ -9,11 +9,12 @@ ProxyFactory.setProvider(web3.currentProvider);
 GnosisSafe.setProvider(web3.currentProvider);
 
 const notOwnedAddress = "0x0000000000000000000000000000000000000002";
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(ProxyFactory, { from: accounts[0] });
-  return deployer.deploy(GnosisSafe, { from: accounts[0] }).then((safe) => {
-    safe.setup([notOwnedAddress], 1, 0, 0, 0, 0, 0, 0)
+  return deployer.deploy(GnosisSafe, { from: accounts[0] }).then(async (safe) => {
+    await safe.setup([notOwnedAddress], 1, ZERO_ADDRESS, '0x', ZERO_ADDRESS, 0, ZERO_ADDRESS, { from: accounts[0] })
     return safe
   });
 };
