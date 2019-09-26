4aa913302324edee42ff79ed37106ee9c1370a42
anon <sarah.anne.friend@gmail.com>
2019-09-26 18:16:45 -0400

    Scripts for testing relayer, migrations using good initial payout

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Hub = artifacts.require("./Hub.sol");
+const { convert } = require('../test/helpers/math');
+
 
 module.exports = async function(deployer, network, accounts) {
-  await deployer.deploy(Hub, accounts[0], 1736111111111111, 0, 'CRC', 100);
+  await deployer.deploy(Hub, accounts[0], 1736111111111111, 0, 'CRC', convert(100));
 };
