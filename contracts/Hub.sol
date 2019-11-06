d8518b3ddebfc9a04869aa02adb499b444602d4c
anon <sarah.anne.friend@gmail.com>
2019-11-06 09:09:38 -0500

    Clean up

--- contracts/Hub.sol
+++ contracts/Hub.sol
@@ -110,10 +110,66 @@ contract Hub {
         return max.sub(userToToken[from].balanceOf(to));
     }
 
-    // Starts with msg.sender then ,
-    // iterates through the nodes list swapping the nth token for the n+1 token
     event Validation(address user, uint sent, uint received);
 
+    // build the data structures we will use for validation
+    // if we haven't seen the addresses, add them to the validation mapping
+    // if we have, increment their sent/received amounts
+    function buildValidationData(address src, address dest, uint wad) internal {
+        if (validation[src].identity != address(0)) {
+            validation[src].sent = validation[src].sent + wad;
+        } else {
+            validation[src].identity = src;
+            validation[src].sent = wad;
+            seen.push(src);
+        }
+        if (validation[dest].identity != address(0)) {
+            validation[dest].received = validation[dest].received + wad;
+        } else {
+            validation[dest].identity = dest;
+            validation[dest].received = wad; 
+            seen.push(dest);   
+        }
+    }
+
+    function validateTransferThrough(uint256 steps) internal {
+        // a valid path has only one true sender and reciever, for all other
+        // addresses in the path, sent = received
+        // also, the sender should be msg.sender
+        address src;
+        address dest;
+        for (uint i = 0; i < seen.length; i++) {
+            transferValidator memory curr = validation[seen[i]];
+            if (curr.sent > curr.received) {
+                require(src == address(0), "Path sends from more than one src");
+                require(curr.identity == msg.sender, "Path doesn't send from transaction sender");
+                src = curr.identity;
+            }
+            if (curr.received > curr.sent) {
+                require(dest == address(0), "Path sends to more than one dest");
+                dest = curr.identity;
+            }
+        }
+        // sender should not recieve, recipient should not send
+        require(validation[src].received == 0, "Sender is receiving");
+        require(validation[dest].sent == 0, "Recipient is sending");
+        // the total amounts sent and received by src and dest should match
+        require(validation[src].sent == validation[dest].received, "Unequal sent and received amounts");
+        // the maximum amount of addresses we should see is one more than steps in the path
+        require(seen.length <= steps + 1, "Seen too many addresses");
+        // clean up the validation datastructures
+        for (uint i = seen.length; i >= 1; i--) {
+            validation[seen[i-1]].sent = 0;
+            validation[seen[i-1]].received = 0;
+            validation[seen[i-1]].identity = address(0);
+            seen.pop();
+        }
+        // sanity check that we cleaned everything up correctly
+        require(seen.length == 0, "Seen should be empty");
+    }
+
+    // Walks through tokenOwners, srcs, dests, and amounts array and
+    // executes transtive transfer - also validates path
     function transferThrough(address[] memory tokenOwners, address[] memory srcs, address[] memory dests, uint[] memory wads) public {
         require(srcs.length <= 5, "Too complex path");
         require(dests.length == tokenOwners.length, "Tokens array length must equal dests array" );
@@ -124,53 +180,15 @@ contract Hub {
             address token = tokenOwners[i];
             uint256 wad = wads[i];
             
+            // check that no trust limits are violated
             uint256 max = checkSendLimit(token, dest);
             require(userToToken[token].balanceOf(dest) + wad <= max, "Trust limit exceeded");
 
-            if (validation[src].identity != address(0)) {
-                validation[src].sent = validation[src].sent + wad;
-            } else {
-                validation[src].identity = src;
-                validation[src].sent = wad;
-                seen.push(src);
-            }
-            if (validation[dest].identity != address(0)) {
-                validation[dest].received = validation[dest].received + wad;
-            } else {
-                validation[dest].identity = dest;
-                validation[dest].received = wad; 
-                seen.push(dest);   
-            }
+            buildValidationData(src, dest, wad);
 
             userToToken[token].hubTransfer(src, dest, wad);
         }
-        address src1;
-        address dest2;
-        for (uint i = 0; i < seen.length; i++) {
-            transferValidator memory curr = validation[seen[i]];
-            emit Validation(curr.identity, curr.sent, curr.received);
-            if (curr.sent > curr.received) {
-                require(src1 == address(0), "Path sends from more than one src");
-                require(curr.identity == msg.sender, "Path doesn't send from transaction sender");
-                src1 = curr.identity;
-            }
-            if (curr.received > curr.sent) {
-                require(dest2 == address(0), "Path sends to more than one dest");
-                dest2 = curr.identity;
-            }
-        }
-        emit Validation(validation[src1].identity, validation[src1].sent, validation[src1].received);
-        emit Validation(validation[dest2].identity, validation[dest2].sent, validation[dest2].received);
-        require(validation[src1].received == 0, "Sender is receiving");
-        require(validation[dest2].sent == 0, "Recipient is sending");
-        require(validation[src1].sent == validation[dest2].received, "Unequal sent and received amounts");
-        require(seen.length <= srcs.length + 1, "Seen too many addresses");
-        for (uint i = seen.length; i >= 1; i--) {
-            validation[seen[i-1]].sent = 0;
-            validation[seen[i-1]].received = 0;
-            seen.pop();
-        }
-        require(seen.length == 0, "Seen should be empty");
+        validateTransferThrough(srcs.length);
     }
 
 }
