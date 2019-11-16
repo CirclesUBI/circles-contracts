9e613b1ce6a82816b2b01312121118984a5b63c2
anon <sarah.anne.friend@gmail.com>
2019-11-16 15:02:01 -0500

    Issuance as compound interest tests

--- test/UBI.js
+++ test/UBI.js
@@ -3,6 +3,7 @@ const truffleContract = require('truffle-contract');
 const { BigNumber, ZERO_ADDRESS, decimals } = require('./helpers/constants');
 const { bn, convertToBaseUnit } = require('./helpers/math');
 const { assertRevert } = require('./helpers/assertRevert');
+const { increase } = require('./helpers/increaseTime');
 
 const Hub = artifacts.require('Hub');
 const Token = artifacts.require('Token');
@@ -15,7 +16,7 @@ contract('UBI', ([_, owner, recipient, anotherAccount, systemOwner]) => { // esl
   let hub = null;
   let token = null;
 
-  const inflation = bn(275);
+  const inflation = bn(107);
   const divisor = bn(100);
   const period = bn(7885000000);
   const symbol = 'CRC';
@@ -57,6 +58,33 @@ contract('UBI', ([_, owner, recipient, anotherAccount, systemOwner]) => { // esl
 
     it('should throw on overflow', async () => {
       await assertRevert(hub.pow(12, 583333333))
+    });
+  });
+
+  describe('issuance', () => {
+    beforeEach(async () => {
+      hub = await Hub.new(systemOwner, inflation, divisor, period, symbol, initialPayout);
+    });
+
+    it('returns the correct issuance at deployment', async () => {
+      (await hub.issuance()).should.be.bignumber.equal(convertToBaseUnit(100));
+    });
+
+    it('returns the correct issuance after 1 period', async () => {
+      await increase(period.toNumber());
+      const q = inflation.pow(bn(1));
+      const d = divisor.pow(bn(1));
+      const compounded = (initialPayout.mul(q)).div(d);
+      (await hub.issuance()).should.be.bignumber.equal(compounded);
+    });
+
+    it('returns the correct issuance after x period', async () => {
+      const time = period.mul(bn(5))
+      await increase(time.toNumber());
+      const q = inflation.pow(bn(5));
+      const d = divisor.pow(bn(5));
+      const compounded = (initialPayout.mul(q)).div(d);
+      (await hub.issuance()).should.be.bignumber.equal(compounded);
     });
   });
 
