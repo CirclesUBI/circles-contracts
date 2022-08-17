2981a1927fa67ab30374979284c913437e97a753
jacque <jacque@jacques-MBP.fritz.box>
2022-08-17 12:56:48 +0200

    adding contracts for local development

--- migrations/3_deploy_safe.js
+++ migrations/3_deploy_safe.js
@@ -1,14 +1,21 @@
 const truffleContract = require('@truffle/contract');
 const proxyArtifacts = require('@circles/safe-contracts/build/contracts/ProxyFactory.json');
 const safeArtifacts = require('@circles/safe-contracts/build/contracts/GnosisSafe.json');
+const multiSendArtifacts = require('@circles/safe-contracts/build/contracts/MultiSend.json');
+const masterCopyArtifact = require('@circles/safe-contracts/build/contracts/MasterCopy.json');
 
 const GnosisSafe = truffleContract(safeArtifacts);
-const ProxyFactory = truffleContract(proxyArtifacts);
-
+const ProxyFactory = truffleContract(proxyArtifacts)
+const MultiSend = truffleContract(multiSendArtifacts)
+const MasterCopy = truffleContract(masterCopyArtifact)
 GnosisSafe.setProvider(web3.currentProvider);
 ProxyFactory.setProvider(web3.currentProvider);
+MultiSend.setProvider(web3.currentProvider);
+MasterCopy.setProvider(web3.currentProvider);
 
 module.exports = async function (deployer, network, accounts) {
-  await deployer.deploy(GnosisSafe, { from: accounts[0] });
+  await deployer.deploy(MultiSend, { from: accounts[0] })
+  await deployer.deploy(MasterCopy, { from: accounts[0] })
+  await deployer.deploy(GnosisSafe, { from: accounts[0] })
   return deployer.deploy(ProxyFactory, { from: accounts[0] });
 };
