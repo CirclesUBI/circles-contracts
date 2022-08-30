f78873c0f2ee497f0a24c655ef2b3bff0f81b093
jacque <jacque@jacques-MBP.home>
2022-08-30 18:42:04 +0200

    restore constant

--- test/helpers/constants.js
+++ test/helpers/constants.js
@@ -1,10 +1,11 @@
 const web3 = require('web3'); // eslint-disable-line import/no-extraneous-dependencies
 
 const BigNumber = web3.utils.BN;
+
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 const decimals = new BigNumber(18);
 const extraGas = 17721975;
-const maxGas = 0x989680;
+const maxGas = 0xfffffffffffff;
 const inflation = new BigNumber(275);
 const period = new BigNumber(7885000000);
 const symbol = 'CRC';
