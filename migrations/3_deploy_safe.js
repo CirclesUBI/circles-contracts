2ab1d86b6f339f52e0cc3f0a4db3634338f25daf
ele <lluna@bitspossessed.org>
2022-11-07 14:04:21 +0100

    Lint

--- migrations/3_deploy_safe.js
+++ migrations/3_deploy_safe.js
@@ -8,10 +8,12 @@ const GnosisSafe = truffleContract(safeArtifacts);
 const ProxyFactory = truffleContract(proxyArtifacts)
 const MultiSend = truffleContract(multiSendArtifacts)
 const MultiSendCallOnly = truffleContract(MultiSendCallOnlyArtifacts)
+
 GnosisSafe.setProvider(web3.currentProvider);
 ProxyFactory.setProvider(web3.currentProvider);
 MultiSend.setProvider(web3.currentProvider);
 MultiSendCallOnly.setProvider(web3.currentProvider);
+
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(MultiSend, { from: accounts[0] });
   await deployer.deploy(GnosisSafe, { from: accounts[0] });
